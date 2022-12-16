078115116bcf4f6535f360e17ba7557b4b6976e2
Ryanb021 <darcknight1980@gmail.com>
2022-12-16 05:20:00 +0000

    Changes

---
+++ app.js
@@ -0,0 +1,14 @@
+function askUser(){
+let intention = prompt("Will you be punching someone today? Y/N");
+if (intention == ""){
+    intention = prompt{"Y or N answer is all I need really"};
+if (intention==Y){
+    console.log("Have fun punching that person!");
+}
+if (intention==N){
+    console.log("Aww, maybe next time you will decide to punch that person.");
+}
+}
+    }
+
+askUser{};
