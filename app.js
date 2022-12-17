ffbdfb16e12a8073058203320a1e90c22b485f68
Ryanb021 <darcknight1980@gmail.com>
2022-12-17 03:31:03 +0000

    chnages

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-function askUser(){
+function pic(){
+let picture = prompt("Guess how many fists you'll see in my page. Choose between 1 to 5");
+	picture = prompt("Guess how many fists you'll see in my page. Choose from 1 to 5");
+}
+while picture (picture>5 || picture<1){
+	alert("Cmon I said between 1 to 5!!!")
+	rating = prompt("Guess how many fists you'll see in my page. Choose between 1 to 5");
+}
 
-    var do = "Y";
-    var response;
-
-    while(response != do){
-        respnse = window.prompt("Will you be punching someone today?");
-    
-    }
-    alert("Have fun punching that person!");
-    }
-
-askUser();
+for let (let i=0;i<picture; i++){
+	document.write("<img src='https://images.pexels.com/photos/635356/pexels-photo-635356.jpeg?cs=srgb&dl=pexels-neosiam-635356.jpg&fm=jpg' alt='fist' width='15' height='20/>");
+}
