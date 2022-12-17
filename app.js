fd76ef40ec5d7c5dbbb8af100feeef7216fc398b
Ryanb021 <darcknight1980@gmail.com>
2022-12-17 04:55:09 +0000

    C

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-function howManyLives() {
-    let numOfLives = prompt('How many lives do you want to start with? (1-5)')
-    while (numOfLives > 5 || numOfLives <= 0) {
-        numOfLives = prompt('Please reenter how many lives you want to start with. (1-5)')
+function howmanyfist() {
+    let numFist = prompt('Guess how many fist you will see here? Choose between 1 to 5')
+    while (numFist > 5 || numFist <= 0) {
+        numFist = prompt('I said between 1 to 5!!!)')
     };
 
-    for (let i = 0; i < numOfLives; i++) {
-        document.write(
-            "<img width='30px' height='30px' src='https://orig04.deviantart.net/253e/f/2015/073/1/b/pink_pixel_heart_gif_by_kerostudio-d8fohut.gif' alt='pixel hearts'/>"
-        );
+    for (let i = 0; i < numFist; i++){
+        document.write("<img width='30px' height='30px' src='https://raw.githubusercontent.com/jamesCodes808/hello-world/main/campfire.png'/>");
     }
