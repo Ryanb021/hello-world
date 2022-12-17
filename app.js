4a9429bc0eda88cb799b2d8b4a8fac309fc35878
Ryanb021 <darcknight1980@gmail.com>
2022-12-17 06:10:10 +0000

    Changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function howmanyfist() {
+    function howmanyfist() {
     let numFist = prompt('Guess how many fist you will see here? Choose between 1 to 5')
     while (numFist > 5 || numFist <= 0) {
         numFist = prompt('I said between 1 to 5!!!)')
@@ -6,4 +6,4 @@ function howmanyfist() {
 
     for (let i = 0; i < numFist; i++){
         document.write("<img width='30px' height='30px' src='https://raw.githubusercontent.com/jamesCodes808/hello-world/main/campfire.png'/>");
-    }
+    };
