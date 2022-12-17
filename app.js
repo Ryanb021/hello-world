b6073aeff9b849ab23ff6752224420e5caa7d916
Ryanb021 <darcknight1980@gmail.com>
2022-12-17 04:05:13 +0000

    Changes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+body{
 function pic(){
 let picture = prompt("Guess how many fists you'll see in my page. Choose between 1 to 5");
 	picture = prompt("Guess how many fists you'll see in my page. Choose from 1 to 5");
@@ -9,4 +10,5 @@ while picture (picture>5 || picture<1){
 
 for let (let i=0;i<picture; i++){
 	document.write("<img src='https://images.pexels.com/photos/635356/pexels-photo-635356.jpeg?cs=srgb&dl=pexels-neosiam-635356.jpg&fm=jpg' alt='fist' width='15' height='20/>");
+}
 }
