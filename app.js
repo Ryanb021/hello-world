af282f97dd6e20162e1e9982f2fc3a875e7bf57a
Ryan Bagan <darcknight1980@gmail.com>
2023-01-08 21:44:10 -0800

    Changes

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
-let newFunction = function (){
-	console.log("Hi from the new function");
-  }	
-  newFunction()
-   let choice = confirm("You have to punch someone when entering this site");
+let choice = confirm("You have to punch someone when entering this site");
     if (choice == false) {
-        window.location.assign('https://xkcd.com/');
-		};
-		    let choose = confirm("You also need to kick someone in the bajewels when entering this site");
-    if (choose == false) {
-      window.location.assign('https://theoatmeal.com/blog/justdoitlater');
-	};
-	function howmanyfist() {
-    let numFist = prompt('Guess how many fist you will see here? Choose between 1 to 5')
-    while (numFist > 5 || numFist <= 0) {
-        numFist = prompt('I said between 1 to 5!!!)')
-    };
+        window.location.assign('https://xkcd.com/');}
 
-    for (let i = 0; i < numFist; i++){
-        document.write("<img width='30px' height='30px' src='https://raw.githubusercontent.com/jamesCodes808/hello-world/main/campfire.png'/>");
-    };
-	var ask = "Yes";
+    let choose = confirm("You also need to kick someone in the bajewels when entering this site");
+    if (choose == false) {
+      window.location.assign('https://theoatmeal.com/blog/justdoitlater');}
+
+   var fist = "5";
+var response;
+while(response !=fist){
+    response = window.prompt("Guess how many fists are in this page? Choose between 1 to 5.");}
+
+var ask = "Y";
 var response;
 while(response !=ask){
-    response = window.prompt("So... are you going to punch someone today? Yes or No");
+    response = window.prompt("So... are you going to punch someone today? Y/N?");
 }
 alert("Have fun punching that person!");
